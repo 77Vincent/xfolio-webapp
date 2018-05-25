d57a678d1e21aedfa22486cd7aed04f62fdbb13d
sqxieshuai <sqxieshuai@gmail.com>
2018-05-25 23:31:00 +0800

    feat: 成为登录页

--- app/pages/SignIn/index.jsx
+++ app/pages/SignIn/index.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import _ from 'lodash'
+import { Form, Input, Button } from 'antd'
+import { Link } from 'react-router-dom'
 
 import './index.less'
 
@@ -26,7 +28,38 @@ export default class SignIn extends Component {
 
     return (
       <div className="sign-in-wrap" style={wrapStyle}>
-        sign-in-wrap
+        <h2 className="page-title">登录</h2>
+        <Form layout="horizontal">
+          <Form.Item
+            label="手机号码"
+            colon={false}
+            labelCol={{
+              span: 5,
+            }}
+            wrapperCol={{
+              span: 18,
+            }}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="账号密码"
+            colon={false}
+            labelCol={{
+              span: 5,
+            }}
+            wrapperCol={{
+              span: 18,
+            }}
+          >
+            <Input />
+          </Form.Item>
+          <div className="account-opera-buttons">
+            <Link to="/forgot">忘记密码</Link>
+            <Link to="/signup">注册用户</Link>
+          </div>
+          <Button className="button-signin">登录</Button>
+        </Form>
       </div>
     )
   }
