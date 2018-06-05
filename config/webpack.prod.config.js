098cb8e539b1a7259a6b1b67983e029b1f348f2c
sqxieshuai <sqxieshuai@gmail.com>
2018-06-05 13:33:25 +0800

    chore: 修改 webpack 中引入 uglifyjs-webpack-plugin 的方式

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 const webpackConfigBase = require('./webpack.base.config')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const webpackConfigProd = {
   plugins: [
@@ -13,7 +14,7 @@ const webpackConfigProd = {
     // 根据入口文件，提取重复引用的公共代码类库，打包到单独文件中
     // new webpack.optimize.OccurenceOrderPlugin(),
     /* 压缩优化代码开始*/
-    new webpack.optimize.UglifyJsPlugin({ minimize: true }),
+    new UglifyJsPlugin({}),
   ],
 }
 
