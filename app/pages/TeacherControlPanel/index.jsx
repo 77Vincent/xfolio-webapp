5ebd64e6f7f17e28ae23c91c35bf14852fb6d0ac
sqxieshuai <sqxieshuai@gmail.com>
2018-06-05 13:47:53 +0800

    refactor: 控制面板菜单栏在页面滚动后固定位置
    fix #15

--- app/pages/TeacherControlPanel/index.jsx
+++ app/pages/TeacherControlPanel/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import _ from 'lodash'
 import { NavLink } from 'react-router-dom'
+import { Anchor } from 'antd'
 
 import './index.less'
 
@@ -30,12 +31,19 @@ export default class TeacherControlPanel extends Component {
     return (
       <div className="teacher-control-panel" style={wrapStyle}>
         <div className="menus-wrap">
-          <NavLink to="/dashboard/profile" className="menu-item" activeClassName="current">基本信息</NavLink>
-          <NavLink to="/dashboard/profile/edit" className="menu-item" activeClassName="current">编辑主页</NavLink>
-          <NavLink to="/dashboard/notification" className="menu-item" activeClassName="current">我的消息</NavLink>
-          <NavLink to="/dashboard/plan" className="menu-item" activeClassName="current">课程安排</NavLink>
-          <NavLink to="/dashboard/price" className="menu-item" activeClassName="current">时间&价格</NavLink>
-          <NavLink to="/dashboard/account" className="menu-item" activeClassName="current">我的账户</NavLink>
+          <Anchor
+            showInkInFixed={false}
+            getContainer={() => (document.body)}
+          >
+            <div className="menu-list-wrap">
+              <NavLink to="/dashboard/profile" className="menu-item" activeClassName="current">基本信息</NavLink>
+              <NavLink to="/dashboard/profile/edit" className="menu-item" activeClassName="current">编辑主页</NavLink>
+              <NavLink to="/dashboard/notification" className="menu-item" activeClassName="current">我的消息</NavLink>
+              <NavLink to="/dashboard/plan" className="menu-item" activeClassName="current">课程安排</NavLink>
+              <NavLink to="/dashboard/price" className="menu-item" activeClassName="current">时间&价格</NavLink>
+              <NavLink to="/dashboard/account" className="menu-item" activeClassName="current">我的账户</NavLink>
+            </div>
+          </Anchor>
         </div>
         <div className="content-wrap">
           { React.createElement(this.props.content) }
